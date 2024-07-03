757548385f2c5ab4adb6c6017eb439d4c6972e94
Mikequez12 <142720633+Mikequez12@users.noreply.github.com>
2024-07-03 15:31:26 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,24 @@
+let project_data = {
+    "Simulación de instituto, IA algorítmica":{
+        "url":"simulación-de-instituto-ia",
+        "summary":"Imagina simular un instituto: alumno por alumno, acción por acción. En este proyecto, tendrás una interfaz gráfica donde podrás ver pasar los días como director de un instituto.",
+        "languages":["Python"],
+        "motors":["Editor de texto"],
+        "knowledge":7,
+        "tags":["IA","Algoritmo","Matemáticas","Tkinter"],
+        "verified":true
+    },
+    'TkIpywidgets':{
+        'url': 'https://sites.google.com/view/tff-sites/proyectos/tkipywidgets',
+        'summary': 'Para los amantes de Tkinter, s que es difcil usar Ipywidgets. Es por eso que he decidido crear un equipo donde crearemos un nuevo paquete que genere widgets de Ipywidgets con cdigo de Tkinter.',
+        'languages': ['Python', 'HTML'],
+        'motors': ['Editor de texto'],
+        'knowledge': 8,
+        'tags': ['Tkinter', 'Ipywidgets'],
+        'verified':true
+    }
+}
+
+window.project_data = project_data;
+
+let checked = '<svg fill="var(--pastel-green)" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/></svg>'
